1088f64ad46a9c2ba242b9ed19acc420f7bc628b
bikram <bikram4252@gmail.com>
2012-11-14 16:37:05 +1300

    Rails Application Template for quick starting projects with bootstrap

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,13 @@
+class MainController < ApplicationController
+  def home
+  end
+
+  def shop
+  end
+
+  def deals
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WwwSamplerailsappCom::Application.config.secret_token = '453e63c9843958c7ff923ea6fd6f70a7da8a291245574e73e584a402ecda3fb250761bed47ccdf2e4e68a0d0a9cb1843ce020234ef9fa706c16ff061f21a6811'
